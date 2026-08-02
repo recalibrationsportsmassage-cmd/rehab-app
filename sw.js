6d552b399526cdf0d6f0d421bbcb18ddb1f7a19f
recalibrationsportsmassage-cmd <recalibrationsportsmassage@gmail.com>
2026-08-02 22:34:51 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "recal-v2";
-const SHELL = ["./", "index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png"];
+const CACHE = "recal-v4";
+const SHELL = ["./", "index.html", "icon-192.png", "icon-512.png"];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
